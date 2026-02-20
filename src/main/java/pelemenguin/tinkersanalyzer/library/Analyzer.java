cb62731bb35e8b86371dbb308a31a390cf5ab1ca
Pelemenguin <elepentuin@outlook.com>
2026-02-21 01:45:28 +0800

    Add Javadoc for Analyzer and DisplayAnalyzerGraphModifierHook

--- src/main/java/pelemenguin/tinkersanalyzer/library/Analyzer.java
+++ src/main/java/pelemenguin/tinkersanalyzer/library/Analyzer.java
@@ -11,15 +11,46 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 
+/**
+ * A class for collecting {@link pelemenguin.tinkersanalyzer.client.graph.AnalyzerGraph AnalyzerGraph}s to be added to the screen.
+ */
 public class Analyzer {
 
     private HashMap<UUID, Pair<ResourceLocation, CompoundTag>> graphs = new HashMap<>();
     private HashMap<UUID, AnalyzerLayoutEntry> defaultLayouts = new HashMap<>();
 
+    /**
+     * Gets a copy of the map that stored all collected graphs.
+     * 
+     * @return The collected graphs
+     */
     public Map<UUID, Pair<ResourceLocation, CompoundTag>> getAllGraphs() {
         return Map.copyOf(graphs);
     }
 
+    /**
+     * Create or modify the data of a {@link pelemenguin.tinkersanalyzer.client.graph.AnalyzerGraph AnalyzerGraph}.
+     *
+     * <p>
+     * If a {@link java.util.UUID UUID} does not have an AnalyzerGraph added,
+     * a new {@link net.minecraft.nbt.CompoundTag CompoundTag} will be created and returned.
+     * Add the necessary data in this CompoundTag to provide information for creating the graph.
+     *
+     * <p>
+     * If an added AnalyzerGraph is already using this UUID, the previously stored CompoundTag for that graph will be returned.
+     * You can continue to modify the data in this CompoundTag.
+     *
+     * <p>
+     * Different types of AnalyzerGraphs require different data.
+     * For the AnalyzerGraph base class, it requires an integer property {@code color} to determine the background color.
+     * Subclasses of other AnalyzerGraphs may need different data and should provide it as needed.
+     *
+     * @param uuid          The UUID of the graph
+     * @param graphId       A {@link net.minecraft.resources.ResourceLocation ResourceLocation} representing the type of AnalyzerGraph.
+     *                      It should be registered in {@link pelemenguin.tinkersanalyzer.client.AnalyzerOverlay AnalyzerOverlay} in advance
+     * @param defaultLayout The default layout of the graph
+     * @return              A CompoundTag representing the data of the AnalyzerGraph
+     */
     public CompoundTag createOrGetGraphData(UUID uuid, ResourceLocation graphId, AnalyzerLayoutEntry defaultLayout) {
         Pair<ResourceLocation, CompoundTag> pair = graphs.get(uuid);
         CompoundTag data;
@@ -34,6 +65,11 @@ public class Analyzer {
         return data;
     }
 
+    /**
+     * An overload of createOrGetGraphData without parameter {@code defaultLayout}.
+     * 
+     * @see #createOrGetGraphData(UUID, ResourceLocation, AnalyzerLayoutEntry)
+     */
     public CompoundTag createOrGetGraphData(UUID uuid, ResourceLocation graphId) {
         return this.createOrGetGraphData(uuid, graphId, new AnalyzerLayoutEntry(0.0f, 0.0f, 192.0f));
     }
--- src/main/java/pelemenguin/tinkersanalyzer/library/hook/DisplayAnalyzerGraphModifierHook.java
+++ src/main/java/pelemenguin/tinkersanalyzer/library/hook/DisplayAnalyzerGraphModifierHook.java
@@ -9,8 +9,18 @@ import slimeknights.tconstruct.library.modifiers.ModifierHooks;
 import slimeknights.tconstruct.library.module.ModuleHook;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
+/**
+ * Hook for adding analyzer graphs to the {@link pelemenguin.tinkersanalyzer.client.AnalyzerOverlay AnalyzerOverlay} when rendering.
+ */
 public interface DisplayAnalyzerGraphModifierHook {
 
+    /**
+     * Add new analyzer graphs to the {@link pelemenguin.tinkersanalyzer.client.AnalyzerOverlay AnalyzerOverlay}.
+     * 
+     * @param tool     Tool instance
+     * @param modifier The modifier and level
+     * @param analyzer An {@link pelemenguin.tinkersanalyzer.library.Analyzer Analyzer} instance for collecting analyzer graphs
+     */
     void addGraph(IToolStackView tool, ModifierEntry modifier, Analyzer analyzer);
 
     record AllMerger(Collection<DisplayAnalyzerGraphModifierHook> modules) implements DisplayAnalyzerGraphModifierHook {
@@ -22,6 +32,9 @@ public interface DisplayAnalyzerGraphModifierHook {
         }
     }
 
+    /**
+     * The {@link slimeknights.tconstruct.library.module.ModuleHook ModuleHook} instance for being added when registering modifiers.
+     */
     public static final ModuleHook<DisplayAnalyzerGraphModifierHook> INSTANCE = ModifierHooks.register(
             TinkersAnalyzer.makeResource("display_analyzer_graph"),
             DisplayAnalyzerGraphModifierHook.class,
