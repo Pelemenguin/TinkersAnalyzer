166e72f4352ac032005889cdb3f39cffc7af0d0e
Pelemenguin <elepentuin@outlook.com>
2026-02-28 18:56:01 +0800

    Add tracking entity display on EntityRadarGraph

--- src/main/java/pelemenguin/tinkersanalyzer/client/graph/EntityRadarGraph.java
+++ src/main/java/pelemenguin/tinkersanalyzer/client/graph/EntityRadarGraph.java
@@ -1,21 +1,30 @@
 package pelemenguin.tinkersanalyzer.client.graph;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.registries.ForgeRegistries;
 import pelemenguin.tinkersanalyzer.client.graph.element.RadarGraphElement;
+import pelemenguin.tinkersanalyzer.client.graph.element.TaggedTextGraphElement;
 import pelemenguin.tinkersanalyzer.content.TinkersAnalyzerModifiers;
 
 public class EntityRadarGraph extends AnalyzerGraph {
 
     protected RadarGraphElement element;
+    protected EntityType<?> tracking;
 
     public EntityRadarGraph(CompoundTag tag) {
         super(tag);
 
         this.elements.clear();
         this.addElement(this.element);
+        if (this.tracking != null) {
+            TaggedTextGraphElement text = new TaggedTextGraphElement(this.tracking::getDescription).colored(this.color).tagBelow(Component.literal("Tracking"));
+            text.x = this.element.getWidth() + 1;
+            text.scale = 2;
+            this.addElement(text);
+        }
     }
 
     @Override
@@ -24,11 +33,14 @@ public class EntityRadarGraph extends AnalyzerGraph {
         if (this.element == null) {
             this.element = new RadarGraphElement(15).color(this.color);
         }
-        ResourceLocation requiredType = ResourceLocation.tryParse(tag.getString("entityType"));
+        String entityName = tag.getString("entityType");
+        if (entityName.isEmpty()) return;
+        ResourceLocation requiredType = ResourceLocation.tryParse(entityName);
         if (requiredType == null) return;
         EntityType<?> type = ForgeRegistries.ENTITY_TYPES.getValue(requiredType);
         if (type == null) return;
         this.element.trackEntity(type);
+        this.tracking = type;
     }
 
 }
